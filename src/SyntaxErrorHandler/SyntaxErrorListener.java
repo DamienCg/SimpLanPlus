82e0bdecdd2a1e8a71c7df6973e98c72effbed9c
TommasoSegato <65330555+TommasoSegato@users.noreply.github.com>
2022-03-20 20:13:52 +0100

    Add files via upload

--- src/SyntaxErrorHandler/SyntaxErrorListener.java
+++ src/SyntaxErrorHandler/SyntaxErrorListener.java
@@ -2,6 +2,9 @@ package SyntaxErrorHandler;
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +17,26 @@ public class SyntaxErrorListener extends BaseErrorListener {
 
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+
         errorMessages.add("line " + line + ":" + charPositionInLine + " " + msg);
     }
 
+
     public void printError(){
         System.out.println("errorMessages: " + errorMessages);
+    }
+
+
+    public void saveErrorFile() throws FileNotFoundException {
+
+        try{
+            PrintWriter out = new PrintWriter("ErrorFile.txt");
+            out.println("Errore: "+ errorMessages);
+            out.close();
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
     }
 
 }
