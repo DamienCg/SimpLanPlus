6f05a0e6ff8a847cbebc2d78b7ed9eb3b8fb84f7
TommasoSegato <65330555+TommasoSegato@users.noreply.github.com>
2022-03-20 20:12:44 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,28 @@ import ParserAndLexer.SimpLanPlusParser;
 import SyntaxErrorHandler.SyntaxErrorListener;
 import org.antlr.v4.runtime.*;
 
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
         String source = "{ int a; int b; int c = 1 ;\n" +
-                "     if (c > 1 { b = c ; } else { a = b  }\n" +
+                "     if (c > 1 { b[[ = c ; } else { a = b  }\n" +
                 "   }";
 
         SimpLanPlusLexer lexer = new SimpLanPlusLexer(CharStreams.fromString(source));
         SimpLanPlusParser parser = new SimpLanPlusParser(new CommonTokenStream(lexer));
 
-        SyntaxErrorListener MyErrorListner = new SyntaxErrorListener();
-        parser.addErrorListener(MyErrorListner);
+        SyntaxErrorListener MyErrorListener = new SyntaxErrorListener();
+        //parser.removeErrorListeners();
+        parser.addErrorListener(MyErrorListener);
         parser.block();
-        MyErrorListner.printError();
+        MyErrorListener.printError();
+
+        try {
+            MyErrorListener.saveErrorFile();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
