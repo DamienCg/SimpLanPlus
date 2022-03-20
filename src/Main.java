5e54c32a7018b8be0e2f365d8e6b12a471e55fa4
Dcg <damienciagola@gmail.com>
2022-03-20 21:29:50 +0100

    ok
    ok

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,28 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        String source = "{ int a; int b; int c = 1 ;\n" +
-                "     if (c > 1 { b[[ = c ; } else { a = b  }\n" +
-                "   }";
+        try {
+            String source = "{ int a; int b; int c = 1 ;\n" +
+                    "     if (c > 1 { b = c ; } else { a = b  }\n" +
+                    "   }";
 
-        SimpLanPlusLexer lexer = new SimpLanPlusLexer(CharStreams.fromString(source));
-        SimpLanPlusParser parser = new SimpLanPlusParser(new CommonTokenStream(lexer));
+            SimpLanPlusLexer lexer = new SimpLanPlusLexer(CharStreams.fromString(source));
+            SimpLanPlusParser parser = new SimpLanPlusParser(new CommonTokenStream(lexer));
 
-        SyntaxErrorListener MyErrorListener = new SyntaxErrorListener();
-        //parser.removeErrorListeners();
-        parser.addErrorListener(MyErrorListener);
-        parser.block();
-        MyErrorListener.printError();
+            SyntaxErrorListener MyErrorListener = new SyntaxErrorListener();
+            //parser.removeErrorListeners();
+            parser.addErrorListener(MyErrorListener);
+            parser.block();
+            MyErrorListener.printError();
 
         try {
             MyErrorListener.saveErrorFile();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
